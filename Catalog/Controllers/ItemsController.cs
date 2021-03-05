e4a2fa0b97ec1754cbc6542d6ec5345b490ad439
Wando Schneider <wandoschneider@MacBook-Air-de-Wando.local>
2021-03-05 14:24:36 -0300

    Commit inicial da estrutura base da API, ainda sem persistência

---
+++ Catalog/Controllers/ItemsController.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Catalog.Dtos;
+using Catalog.Entities;
+using Catalog.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly IItemsRepository _repository;
+
+        public ItemsController(IItemsRepository repository)
+        {
+            this._repository = repository;
+        }
+
+        [HttpGet]
+        public IEnumerable<ItemDto> GetItems()
+        {
+            var items = _repository.GetItems().Select(item => item.AsDto());
+            return items;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<ItemDto> GetItem(Guid id)
+        {
+            var item = _repository.GetItem(id);
+
+            if (item is null)
+                return NotFound();
+
+            return Ok(item.AsDto());
+        }
+
+        [HttpPost]
+        public ActionResult<ItemDto> CreateItem(CreateItemDto itemDto)
+        {
+            Item item = new()
+            {
+                Id = Guid.NewGuid(),
+                Name = itemDto.Name,
+                Price = itemDto.Price,
+                CreatedDate = DateTimeOffset.UtcNow
+            };
+
+            _repository.CreateItem(item);
+
+            return CreatedAtAction(nameof(GetItem), new {id = item.Id}, item.AsDto());
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult UpdateItem(Guid id, UpdateItemDto itemDto)
+        {
+            var existingItem = _repository.GetItem(id);
+
+            if(existingItem is null)
+                return NotFound();
+
+            Item updatedItem = existingItem with
+            {
+                Name = itemDto.Name,
+                Price = itemDto.Price
+            };
+
+            _repository.UpdateItem(updatedItem);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteItem(Guid id)
+        {
+            var existingItem = _repository.GetItem(id);
+
+            if (existingItem is null)
+                return NotFound();
+
+            _repository.DeleteItem(id);
+            return NoContent();
+        }
+    }
+}
